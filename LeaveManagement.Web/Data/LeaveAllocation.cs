8d67b84fb7de471f18fc279ebaf4c59516289ca3
joeface71 <joefacenda@yahoo.com>
2022-10-09 14:59:27 -0400

    Additional databases added

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }//EFCore uses naming of this property to create a foreign key
+
+        public string EmployeeId { get; set; }
+
+    }
+}
